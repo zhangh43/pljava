5a802a22ae1a35ddf095ccb5af0d486e96c9e359
Alexey Grishchenko <agrishchenko@pivotal.io>
2016-07-07 10:25:53 +0100

    Adding PL/Java build on CentOS 7

---
+++ concourse/scripts/pljava_gpdb_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+set -e
+
+GPDBBIN=$1
+OUTPUT=$2
+OSVER=$3
+
+cp $GPDBBIN/$GPDBBIN.tar.gz /usr/local
+pushd /usr/local
+tar zxvf $GPDBBIN.tar.gz
+popd
+source /usr/local/greenplum-db/greenplum_path.sh
+
+pushd pljava_src
+make clean
+make
+pushd gpdb/packaging
+make cleanall && make
+popd
+popd
+
+cp pljava_src/gpdb/packaging/pljava-*.gppkg $OUTPUT/pljava-$OSVER.gppkg
