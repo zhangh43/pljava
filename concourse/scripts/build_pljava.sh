bc8ca73a09f866ad8f70a980d3e3ca71cba2bfc6
Mike Roth <mroth@pivotal.io>
2017-05-14 16:09:08 -0400

    Integration with gpdb5 release pipeline

---
+++ concourse/scripts/build_pljava.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -x
+set -e
+
+
+if [ "$OSVER" == "centos5" ]; then
+    rm -f /usr/bin/python && ln -s /usr/bin/python26 /usr/bin/python
+fi
+
+mkdir /usr/local/greenplum-db-devel
+tar zxvf bin_gpdb/bin_gpdb.tar.gz -C /usr/local/greenplum-db-devel
+source /usr/local/greenplum-db-devel/greenplum_path.sh
+
+source pljava_src/concourse/scripts/mvn_utils.sh
+mvn_repo_install $OSVER
+
+pushd pljava_src
+make clean
+make
+pushd gpdb/packaging
+make cleanall && make
+popd
+popd
+
+cp pljava_src/gpdb/packaging/pljava-*.gppkg plr_gppkg
