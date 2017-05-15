4d392286e870e7fb55ce90115285acce0e258bd2
Mike Roth <mroth@pivotal.io>
2017-05-15 10:23:54 -0400

    Added TZ handling to optimizer answer file

--- concourse/scripts/release_pljava_install_test.sh
+++ concourse/scripts/release_pljava_install_test.sh
@@ -14,6 +14,11 @@ cd $WORKDIR
 # Install PL/Container package
 gppkg -i $PLJAVABIN/pljava-*.gppkg || exit 1
 
+# Ensure that the JVM is reporting time/dates in UTC to avoid
+# failures during testing (SuSE reports GMT instead of UTC)
+
+
+
 rm -rf $TMPDIR
 cp -r pljava_src $TMPDIR
 cd $TMPDIR
@@ -21,5 +26,6 @@ if [ "$OSVER" == "suse11" ]; then
 #  suse outputs the time using GMT redhat uses UTC, added spaces to not
 #  change TIMEZONE=UTC in the expected file
     sed -i 's/ UTC/ GMT/g' /tmp/localplccopy/gpdb/tests/expected/pljava_test.out
+    sed -i 's/ UTC/ GMT/g' /tmp/localplccopy/gpdb/tests/expected/pljava_test_optimizer.out
 fi
 make targetcheck || exit 1
