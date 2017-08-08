c59bf5376db59a75023a35e0d7f8c720053fa0fe
Haozhou Wang <hawang@pivotal.io>
2017-08-08 15:12:22 +0800

    add release job for concourse

--- concourse/scripts/test_pljava.sh
+++ concourse/scripts/test_pljava.sh
@@ -118,24 +118,6 @@ function prepare_test(){
 
 function test() {
 	su gpadmin -c "bash /home/gpadmin/test.sh $(pwd)"
-
-    mkdir -p pljava_gppkg
-
-    case "$OSVER" in
-        suse11)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.4.2-gp5-sles11-x86_64.gppkg
-        echo "PL/Java test succeeded"
-        ;;
-        centos6)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.4.2-gp5-rhel6-x86_64.gppkg
-        echo "PL/Java test succeeded"
-        ;;
-        centos7)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.4.2-gp5-rhel7-x86_64.gppkg
-        echo "PL/Java test succeeded"
-        ;;
-        *) echo "Unknown OS: $OSVER"; exit 1 ;;
-    esac
 }
 
 function setup_gpadmin_user() {
