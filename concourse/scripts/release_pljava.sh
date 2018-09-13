99411c73b9c2384fca3ce2bd93c4a23c0c4aecfb
Hubert Zhang <hubertzhang@apache.org>
2018-09-13 11:17:25 +0800

    Get release version from git tag
    Co-authored-by: Hao Wu <gfphoenix78@gmail.com>

--- concourse/scripts/release_pljava.sh
+++ concourse/scripts/release_pljava.sh
@@ -6,17 +6,24 @@ CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_DIR=${CWDIR}/../../../
 
 function release() {
+    pushd pljava_src
+    git checkout gpdb5_release
+    if git describe --tags >/dev/null 2>&1 ; then
+        echo "git describe failed" || exit 1
+    fi
+    PLJAVA_VERSION=$(git describe --tags | awk -F. '{printf("%d.%d.%d", $1, $2, $3)}') 
+    popd
     mkdir -p pljava_gppkg
 
     case "$OSVER" in
         suse11)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.4.2-gp5-sles11-x86_64.gppkg
+        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-${PLJAVA_VERSION}-gp5-sles11-x86_64.gppkg
         ;;
         centos6)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.4.2-gp5-rhel6-x86_64.gppkg
+        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-${PLJAVA_VERSION}-gp5-rhel6-x86_64.gppkg
         ;;
         centos7)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.4.2-gp5-rhel7-x86_64.gppkg
+        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-${PLJAVA_VERSION}-gp5-rhel7-x86_64.gppkg
         ;;
         *) echo "Unknown OS: $OSVER"; exit 1 ;;
     esac
